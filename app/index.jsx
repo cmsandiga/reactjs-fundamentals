b02e48bc2e793acd48e3868de570f467db6d073e
Cristian Makoto <cmsandiga@gmail.com>
2018-12-04 01:01:21 +0000

    initial state

--- app/index.jsx
+++ app/index.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import createReactClass from 'create-react-class'
 
-var X = createReactClass({
-  render: function(){
-    return(<p> X Comp</p>)
-  }
-})
-
 var App = createReactClass({
+  getInitialState: function(){
+    return {
+      profiles: [
+        {name: "Makoto",
+         age: 27},
+
+        {name: "Malu",
+        age:28}
+      ]
+    }
+  },
   render: function(){
     return(
-      <div>
-        <p>Hello Worldsss </p>
-        <X/>
-      </div>        
+      <section>
+        <section>
+          <h1>Profile 1</h1>
+          <p>{this.state.profiles[0].name}</p>
+          <p>{this.state.profiles[0].age}</p>
+        </section>
+        <section>
+          <h1>Profile 2</h1>
+          <p>{this.state.profiles[1].name}</p>
+          <p>{this.state.profiles[1].age}</p>
+        </section>
+      </section>      
     )
   }
 })
 ReactDOM.render(<App />, document.getElementById("app"));
+
