22feca65c1c1ba11c909c27c9775ec16a04fa946
Cristian Makoto <cmsandiga@gmail.com>
2018-12-04 01:27:37 +0000

    adding state and using es6 sintax

--- app/index.jsx
+++ app/index.jsx
@@ -2,34 +2,67 @@ import React from "react";
 import ReactDOM from "react-dom";
 import createReactClass from 'create-react-class'
 
-var App = createReactClass({
-  getInitialState: function(){
-    return {
-      profiles: [
-        {name: "Makoto",
-         age: 27},
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-        {name: "Malu",
-        age:28}
-      ]
-    }
-  },
-  render: function(){
-    return(
-      <section>
-        <section>
-          <h1>Profile 1</h1>
-          <p>{this.state.profiles[0].name}</p>
-          <p>{this.state.profiles[0].age}</p>
-        </section>
-        <section>
-          <h1>Profile 2</h1>
-          <p>{this.state.profiles[1].name}</p>
-          <p>{this.state.profiles[1].age}</p>
-        </section>
-      </section>      
+  render() {
+    let hobbies = this.props.hobbies.map(hobby => {
+      return <li>{hobby}</li>
+    })
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <p>{this.props.name} age is {this.props.age} and {this.props.bio}</p>
+        <h3>Hobbies</h3>
+        <ul>
+          {hobbies}
+        </ul>
+      </div>
     )
   }
-})
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      profiles: [
+        {
+          name: "Makoto",
+          bio: "A programmer",
+          age: 27,
+          hobbies: ["play dota", "swimming"]
+        },
+        {
+          name: "Malu",
+          bio: "A San marcos Student",
+          age: 28,
+          hobbies: ["pilastes", "sleeping"]
+        }
+      ]
+    }
+  }
+  render() {
+    let profiles = this.state.profiles.map(profile => {
+      return (
+        <Profile
+          name={profile.name}
+          age={profile.age}
+          bio={profile.bio} ø
+          hobbies={profile.hobbies} />
+      )
+    })
+    return (
+      <div>
+        {profiles}
+      </div>
+    )
+
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
 
